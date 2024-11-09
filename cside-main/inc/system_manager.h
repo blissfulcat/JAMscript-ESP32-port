502606c8d95cdb0ac2ef964cfe3491b82301075f
blissfulcat <howard.yu@mail.mcgill.ca>
2024-11-09 09:28:04 -0500

    fix header file things

--- cside-main/inc/system_manager.h
+++ cside-main/inc/system_manager.h
@@ -26,9 +26,9 @@ system_manager_t* system_manager_init();
 
 /**
  * @brief Frees memory associated with the system_manager_t struct.
- * @param sm pointer to system_manager_t struct
+ * @param system_manager pointer to system_manager_t struct
  */
-void system_manager_destroy(system_manager_t* sm);
+void system_manager_destroy(system_manager_t* system_manager);
 
 /**
  * @brief Initializes the Wifi module and connects to a preset network.
@@ -36,6 +36,12 @@ void system_manager_destroy(system_manager_t* sm);
  * @retval false If error occured during wifi init
  * @todo How to set the SSID and Password for the wifi network?
 */
-bool system_manager_wifi_init(system_manager_t* sm);
+bool system_manager_wifi_init(system_manager_t* system_manager);
+
+/**
+ * @brief resets memory associated with system_manager_t struct
+ * @param system_manager pointer to system_manager_t struct
+ */
+void reset_system_manager(system_manager_t* system_manager);
 
 #endif
