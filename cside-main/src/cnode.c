6f7e0d7b62af10ae0055e8ea9f98d69c527693b4
Zi <zichen.gao2@mail.mcgill.ca>
2024-11-13 14:57:41 -0500

    feat: rewrote cnode_init(), commented out core init temporarily

--- cside-main/src/cnode.c
+++ cside-main/src/cnode.c
@@ -1,5 +1,6 @@
 #include "cnode.h"
 
+#define PRINT_INIT_PROGRESS // undefine to remove the initiation messages when creating a cnode
 
 cnode_t* cnode_init(int argc, char** argv) {
     /* Dynamically allocate cn */
@@ -7,7 +8,9 @@ cnode_t* cnode_init(int argc, char** argv) {
 
     /* Process args */
     // TODO: args = process_args(argc, argv);
-
+#ifdef PRINT_INIT_PROGRESS
+printf("Initiating system ... \r\n");
+#endif
     /* Init system */
     cn->system_manager = system_manager_init();
 
@@ -16,19 +19,36 @@ cnode_t* cnode_init(int argc, char** argv) {
         cnode_destroy(cn);
         return NULL;
     }
+
+#ifdef PRINT_INIT_PROGRESS
+printf("Initiating Wi-Fi ... \r\n");
+#endif
+    /* Init wifi */
+    if (!system_manager_wifi_init(cn->system_manager)) {
+        printf("Could not initiate Wi-Fi. \r\n");
+        cnode_destroy(cn);
+        return NULL;
+    }
     
     /* Init core */
     uint32_t serial_num = 0; // serial num should be determined by args 
     cn->core_state = core_init(serial_num);
 
-    if (cn->core_state == NULL) {
-        printf("Core creation failed. \r\n");
-        cnode_destroy(cn);
-        return NULL;
-    }
-
-    cn->node_id = cn->core_state->device_id; // Do we really need the node_id field? Its already in core_state
-
+// #ifdef PRINT_INIT_PROGRESS
+// printf("Initiating core ... \r\n");
+// #endif
+//     if (cn->core_state == NULL) {
+//         printf("Core creation failed. \r\n");
+//         cnode_destroy(cn);
+//         return NULL;
+//     }
+//     // Do we really need the node_id field? Its already in core_state
+//     cn->node_id = cn->core_state->device_id; 
+   
+    
+#ifdef PRINT_INIT_PROGRESS
+printf("Initiating Zenoh ... \r\n");
+#endif
     /* Init Zenoh */
     cn->zenoh = zenoh_init();
 
@@ -37,6 +57,20 @@ cnode_t* cnode_init(int argc, char** argv) {
         cnode_destroy(cn);
         return NULL;
     }
+
+#ifdef PRINT_INIT_PROGRESS
+printf("Scouting for JNodes ... \r\n");
+#endif
+    /* Using Zenoh to scout for JNodes */
+    if (!zenoh_scout(cn->zenoh)) {
+        printf("Could not find any JNodes. \r\n");
+        cnode_destroy(cn);
+        return NULL;
+    }
+
+#ifdef PRINT_INIT_PROGRESS
+printf("cnode %lu initialized. \r\n", serial_num);
+#endif
     cn->initialized = true;
     return cn;
 }
