5c8a182f0394c6882b38ed9405a8effd2c537e28
Qihao56 <Qihao56@yahoo.com>
2024-11-15 17:11:04 -0500

    add core init and and destroy function

--- cside-main/src/core.c
+++ cside-main/src/core.c
@@ -2,16 +2,39 @@
 
 
 
-corestate_t* core_init( int serialnum) {
-    return NULL;
+corestate_t* core_init(int serialnum) {
+    // Initialize core
+    corestate_t* cs=(corestate_t*)calloc(1,sizeof(corestate_t));
+    cs->serial_num=serialnum;
+    core_setup(cs);
+    return cs;
+
+    /**
+ * @brief Constructor. Initiates the core. Calls core_setup() to generate serial & node ID
+ * @param serialnum Serial number of the node
+ * @return pointer to corestate_t struct
+*/
+
+
 }
 
 
 void core_destroy(corestate_t *cs) {
-    
+//Frees memory allocated during core_init()
+free(cs->device_id);
+free(cs);
+
 }
 
 
 void core_setup(corestate_t *cs) {
-    
+// No file system so just generating a device ID
+
+//Create device ID and store
+//Generate UUID4 for device_ID    
+
+//char buffer[UUID4_LEN];
+//uuid4_generate(buffer);
+//cs->device_id=strdup(buffer);
+
 }
