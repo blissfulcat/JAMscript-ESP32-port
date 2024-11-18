86d360a2f2abc162939a14abfd01a41bf717287c
Qihao56 <Qihao56@yahoo.com>
2024-11-17 22:26:32 -0500

    added UUID4 generation and storage in core setup unsure if working properly, and uuid4.c has a TODO

--- cside-main/src/core.c
+++ cside-main/src/core.c
@@ -1,6 +1,8 @@
 #include "core.h"
-
-
+#include "esp_system.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "uuid4.h"
 
 corestate_t* core_init(int serialnum) {
     // Initialize core
@@ -21,7 +23,15 @@ corestate_t* core_init(int serialnum) {
 
 void core_destroy(corestate_t *cs) {
 //Frees memory allocated during core_init()
-free(cs->device_id);
+if(cs==NULL){
+    return;
+}
+if(cs->device_id!=NULL){
+    free(cs->device_id);
+}
+if(cs->serial_num!=NULL){
+    free(&cs->serial_num);
+}
 free(cs);
 
 }
@@ -33,8 +43,54 @@ void core_setup(corestate_t *cs) {
 //Create device ID and store
 //Generate UUID4 for device_ID    
 
-//char buffer[UUID4_LEN];
-//uuid4_generate(buffer);
-//cs->device_id=strdup(buffer);
+char buffer[UUID4_LEN];
+uuid4_generate(buffer);
+cs->device_id=strdup(buffer);
+esp_err_t err = nvs_flash_init();
+
+if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND){
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    err = nvs_flash_init();
+}
+ESP_ERROR_CHECK(err);
+
+nvs_handle_t my_handle = cs->device_id;
+err = nvs_open("storage", NVS_READWRITE, &my_handle);
+if (err!=ESP_OK){
+    printf("Error %s opening NVS handle \n", esp_err_to_name(err));
+} else {
+    // Read to find space
+    int index=0;
+    err=nvs_get_i32(my_handle,"index", &index);
+    switch (err) {
+        case ESP_OK:
+            printf("Done\n");
+            printf("Restart counter = %i \n", index);
+            break;
+        case ESP_ERR_NVS_NOT_FOUND:
+            printf("The value is not initialized yet!\n");
+            break;
+        default:
+            printf("Error (%s) reading!\n", esp_err_to_name(err));
+    }
+
+        printf("Updating restart counter in NVS ... ");
+        index++;
+        err = nvs_set_i32(my_handle, "index", index);
+        printf((err != ESP_OK) ? "Failed!\n" : "Done\n");
+
+        // Commit written value.
+        // After setting any values, nvs_commit() must be called to ensure changes are written
+        // to flash storage. Implementations may write to storage at other times,
+        // but this is not guaranteed.
+        printf("Committing updates in NVS ... ");
+        err = nvs_commit(my_handle);
+        printf((err != ESP_OK) ? "Failed!\n" : "Done\n");
+        // Close
+        nvs_close(my_handle);
+}
+
+//Temporary
+cs->device_id=NULL;
 
 }
