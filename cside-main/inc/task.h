268e779808846259d57350ba73e4fcd77a2c66fd
Zichene <zichengao0318@gmail.com>
2025-02-01 00:40:53 -0500

    feat: implemented task functions

--- cside-main/inc/task.h
+++ cside-main/inc/task.h
@@ -5,7 +5,10 @@
 #include <freertos/semphr.h>
 #include <freertos/event_groups.h>
 #include <freertos/task.h>
+#include <stdarg.h>
+#include <string.h>
 #include "command.h"
+#include "utils.h"
 
 /* STRUCTS & TYPEDEFS */
 #define MAX_ARGS 20 ///< Maximum number of arguments 
@@ -29,11 +32,13 @@ typedef struct _task_t
 
 /**
  * @brief Constructor. Initializes the task_t struct. The task_handle, args and return_arg (value) are set to NULL.
+ * @note Does not start the task.
  * @param name string describing name of function
  * @param serial_id id of the function
  * @param return_type enum value describing one of several possible return types of the function
  * @param fn_argsig string, argument signature (see task_t)
  * @returns pointer to initialized task_t struct
+ * @retval NULL if could not allocate
 */
 task_t*     task_create(char* name, uint32_t serial_id, argtype_t return_type, char* fn_argsig);
 
@@ -47,7 +52,7 @@ void        task_destroy(task_t* task);
  * @param task pointer to task_t struct
  * @returns pointer to arguments (arg_t)
 */
-arg_t*      task_get_args(task_t* task); 
+arg_t**      task_get_args(task_t* task); 
 
 /**
  * @brief Set the return argument of the task.
@@ -57,4 +62,11 @@ arg_t*      task_get_args(task_t* task);
 void        task_set_return_arg(task_t* task, arg_t* return_arg);
 
 
+/**
+ * @brief Set the arguments of the task.
+ * @param task pointer to task_t struct
+ * @param num_args number of arguments 
+ * @note This function takes in variable arguments, each of which must be an arg_t* object
+*/
+void        task_set_args(task_t* task, int num_args, ...);
 #endif // __TASK_H__
--- cside-main/src/task.c
+++ cside-main/src/task.c
@@ -1,20 +1,86 @@
 #include "task.h"
+/* PRIVATE FUNCTIONS */
+static  argtype_t    char_to_argtype(char c) {
+    switch (c) {
+        case 'n':
+        return NVOID_TYPE;
+        case 's':
+        return STRING_TYPE;
+        case 'i':
+        return INT_TYPE;
+        case 'f':
+        return DOUBLE_TYPE;
+    }
+    return NULL_TYPE;
+}
 
+
+/* PUBLIC FUNCTIONS */
 task_t*     task_create(char* name, uint32_t serial_id, argtype_t return_type, char* fn_argsig) {
-    return NULL;
+    /* Initialize task_t struct */
+    task_t* task = calloc(1, sizeof(task_t));
+    task->return_arg = calloc(1, sizeof(arg_t));
+
+    if (task == NULL) {
+        printf("Could not allocate dynamically");
+        return NULL;
+    }
+    task->name = name;
+    task->serial_id = serial_id;
+    task->return_arg->type = return_type;
+    task->fn_argsig = fn_argsig;
+    task->is_running = false;
+    return task;
 }
 
 
 void        task_destroy(task_t* task) {
+    /* FREE ALL MEMBERS THAT ARE ALLOCATED USING MALLOC, CALLOC */
+    if (task == NULL || task->return_arg == NULL) return;
+    free(task->return_arg);
+    free(task);
     return;
 }
 
 
-arg_t*      task_get_args(task_t* task) {
-    return NULL;
+arg_t**      task_get_args(task_t* task) {
+    if (task == NULL) return NULL;
+    return task->args;
 }
 
 
 void        task_set_return_arg(task_t* task, arg_t* return_arg) {
+    if (task == NULL || task->return_arg == NULL) return;
+    /* Check that the return type matches */
+    if (return_arg->type != task->return_arg->type) {
+       printf("Return type does not match \r\n");
+       return; 
+    } 
+    task->return_arg->type = return_arg->type;
+    task->return_arg->val = return_arg->val;
+    return;
+}
+
+void        task_set_args(task_t* task, int num_args, ...) {
+    if (task == NULL) return;
+
+    if (strlen(task->fn_argsig) != num_args || num_args >= MAX_ARGS) {
+        printf("Number of arguments passed to task_set_args() does not match fn_argsig length or is too large (MAX: %d)\r\n", MAX_ARGS);
+        return;
+    }
+
+    va_list valist;
+    va_start(valist, num_args);
+
+    for (int i = 0; i < num_args; i++) {
+        arg_t* arg = va_arg(valist, arg_t*);
+        if (char_to_argtype(task->fn_argsig[i]) != arg->type) {
+            printf("Incompatible type passed to task_set_args()\r\n");
+            va_end(valist);
+            return;
+        }
+        task->args[i] = arg;
+    }
+    va_end(valist);
     return;
 }
